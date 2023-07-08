48737ba1ea48ed90a10238aec49b4e2919e48b5f
Farhad16 <fhossain389@gmail.com>
2023-07-08 12:29:41 +0600

    Footer section done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,18 +4,18 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-white py-8">
-      <div className="container mx-auto flex flex-wrap">
+      <div className="container mx-auto flex flex-wrap mt-6">
         {/* Left Column */}
         <div className="w-full lg:w-1/3">
           <img src="/Logo.svg" className="w-24" />
-          <p className="mt-4">
+          <p className="mt-4 text-sm">
             © {new Date().getFullYear()}{" "}
             <span className="text-[#90E900]">Abc</span>. All rights reserved.
           </p>
         </div>
 
         {/* Middle Column */}
-        <div className="w-full lg:w-1/3 mt-6 lg:mt-0">
+        <div className="w-full lg:w-1/3 mt-6 lg:mt-0 flex justify-center flex-col">
           <p className="font-bold text-[#EFE600]">LONDON</p>
           <p className="text-sm">20-22 Wenlock Road, London, N1 7GU</p>
           <p className="text-[#90E900] mt-2">+44 207 1188550</p>
@@ -23,21 +23,37 @@ const Footer = () => {
         </div>
 
         {/* Right Column */}
-        <div className="w-full lg:w-1/3 mt-6 lg:mt-0">
-          <h4 className="text-lg font-bold mb-2">Follow Us</h4>
-          <ul className="flex">
+        <div className="w-full lg:w-1/3 mt-6 lg:mt-0 ">
+          <ul className="flex flex-col sm:justify-center">
             <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-400">
+              <a
+                href="#"
+                className="text-white hover:text-[#90E900] hover:underline"
+              >
                 Facebook
               </a>
             </li>
             <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-400">
+              <a
+                href="#"
+                className="text-white hover:text-[#90E900] hover:underline"
+              >
                 Twitter
               </a>
             </li>
             <li className="mr-4">
-              <a href="#" className="text-white hover:text-gray-400">
+              <a
+                href="#"
+                className="text-white hover:text-[#90E900] hover:underline"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li className="mr-4">
+              <a
+                href="#"
+                className="text-white hover:text-[#90E900] hover:underline"
+              >
                 Instagram
               </a>
             </li>
