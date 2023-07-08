ea023422e35353f687f0f54b458e2b28cd428b40
Farhad16 <fhossain389@gmail.com>
2023-07-08 19:09:29 +0600

    Navbar menu design done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,46 +2,38 @@
 import React, { useState } from "react";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu] = useState(false);
-
-  const handleMenuToggle = () => {
-    setShowMenu(!showMenu);
-  };
+  const [active, setActive] = useState("");
+  const menus = ["home", "services", "projects", "blogs"];
 
   return (
     <nav className="flex items-center justify-between p-4 text-white">
       <div className="flex items-center">
-        <img src="/Logo.svg" className="w-24" />
+        <img src="/Logo.svg" className="sm:w-24 max-w-[100px]" />
       </div>
-      <div className="relative">
-        <button
-          className="p-2 rounded-md bg-gray-700 text-white"
-          onClick={handleMenuToggle}
-        >
-          Menu
-        </button>
-        {showMenu && (
-          <div className="absolute top-full left-0 right-0 bg-gray-800 text-white">
-            <div className="container mx-auto py-4">
-              <ul className="flex flex-col md:flex-row">
-                <li className="p-2 md:p-4">Home</li>
-                <li className="p-2 md:p-4">About</li>
-                <li className="p-2 md:p-4 relative group">
-                  Products
-                  <div className="absolute hidden md:block bg-gray-800 text-white mt-2 py-4 px-8 space-y-4">
-                    <ul>
-                      <li>Category 1</li>
-                      <li>Category 2</li>
-                      <li>Category 3</li>
-                    </ul>
-                  </div>
-                </li>
-                <li className="p-2 md:p-4">Contact</li>
-              </ul>
-            </div>
-          </div>
-        )}
-      </div>
+      <ul className="flex flex-row items-center gap-8 sm:visible invisible">
+        {menus.map((menu) => (
+          <li
+            key={menu}
+            onMouseOver={() => setActive(menu)}
+            className={`capitalize transition duration-300 ease-in-out cursor-pointer group flex flex-col items-center relative py-2 hover:text-[#90E900]${
+              active === menu && " active"
+            } `}
+          >
+            {menu}
+            <span
+              className={`h-[5px] w-[5px] bg-[#90E900] rounded-full absolute bottom-0 group-hover:visible ${
+                active === menu ? "sm:visible" : "sm:invisible"
+              }`}
+            ></span>
+          </li>
+        ))}
+
+        <li>
+          <button className="transition duration-300 ease-in-out text-[#90E900] border border-[#90E900] hover:bg-[#BCF266] px-6 py-3 rounded-[30px] hover:text-[#1E2A37] font-medium">
+            Get In Touch
+          </button>
+        </li>
+      </ul>
     </nav>
   );
 };
