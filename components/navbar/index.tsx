3925d5478f56646e438c87882538a0c4ae4b0f05
Farhad16 <fhossain389@gmail.com>
2023-07-09 18:48:05 +0600

    Navbar issue of changing position fixed

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -37,41 +37,43 @@ const Navbar = () => {
   const open = Boolean(anchorEl);
 
   return (
-    <nav className="w-full flex items-center flex-col sm:flex-row justify-between py-4 text-white gap-4">
-      <div className="flex items-center">
-        <img src="/Logo.svg" className="sm:w-24 max-w-[100px]" />
-      </div>
-      <ul className="flex flex-col items-center gap-2 sm:gap-8 sm:flex-row">
-        {menus.map((menu) => (
-          <li
-            key={menu}
-            onMouseEnter={() => setActive(menu)}
-            className={`capitalize transition duration-300 ease-in-out cursor-pointer group flex flex-col items-center relative py-2 hover:text-[#90E900]${
-              active === menu && " active"
-            } `}
-          >
-            {menu}
-            <span
-              className={`h-[6px] w-[6px] bg-[#90E900] rounded-full absolute bottom-0 group-hover:visible ${
-                active === menu ? "visible" : "invisible"
-              }`}
-              onClick={handleClick}
-            ></span>
-          </li>
-        ))}
+    <>
+      <nav className="w-full flex items-center flex-col sm:flex-row justify-between py-4 text-white gap-4">
+        <div className="flex items-center">
+          <img src="/Logo.svg" className="sm:w-24 max-w-[100px]" />
+        </div>
+        <ul className="flex flex-col items-center gap-2 sm:gap-8 sm:flex-row">
+          {menus.map((menu) => (
+            <li
+              key={menu}
+              onMouseEnter={() => setActive(menu)}
+              className={`capitalize transition duration-300 ease-in-out cursor-pointer group flex flex-col items-center relative py-2 hover:text-[#90E900]${
+                active === menu && " active"
+              } `}
+            >
+              {menu}
+              <span
+                className={`h-[6px] w-[6px] bg-[#90E900] rounded-full absolute bottom-0 group-hover:visible ${
+                  active === menu ? "visible" : "invisible"
+                }`}
+                onClick={handleClick}
+              ></span>
+            </li>
+          ))}
 
-        <li>
-          <button className="transition duration-300 ease-in-out text-[#90E900] border border-[#90E900] hover:bg-[#BCF266] px-6 py-3 rounded-[30px] hover:text-[#1E2A37] font-medium mt-2 sm:mt-0">
-            Get In Touch
-          </button>
-        </li>
-        {activeMegaMenu && (
-          <Popover open={open} anchorEl={anchorEl} handleClose={handleClose}>
-            {activeMegaMenu?.component}
-          </Popover>
-        )}
-      </ul>
-    </nav>
+          <li>
+            <button className="transition duration-300 ease-in-out text-[#90E900] border border-[#90E900] hover:bg-[#BCF266] px-6 py-3 rounded-[30px] hover:text-[#1E2A37] font-medium mt-2 sm:mt-0">
+              Get In Touch
+            </button>
+          </li>
+        </ul>
+      </nav>
+      {activeMegaMenu && (
+        <Popover open={open} anchorEl={anchorEl} handleClose={handleClose}>
+          {activeMegaMenu?.component}
+        </Popover>
+      )}
+    </>
   );
 };
 
